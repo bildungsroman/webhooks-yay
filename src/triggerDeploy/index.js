8d2195d4936668f0b26339200348228b552da63a
Anna Spysz <anna.spysz@stackery.io>
2018-10-30 11:28:52 -0700

    updating function

--- src/triggerDeploy/index.js
+++ src/triggerDeploy/index.js
@@ -1,14 +1,13 @@
-exports.githubWebhookListener = (event, context, callback) => {
+module.exports.githubWebhookListener = (event, context, callback) => {
   const token = process.env.GITHUB_WEBHOOK_SECRET;
   const headers = event.headers;
   const body = JSON.parse(event.body);
-  const { repository, pusher } = body;
+  const { repository, sender } = body;
   const repo = repository.name;
-  const username = pusher.name;
+  const username = sender.login;
   const url = repository.url;
   const githubEvent = headers['X-GitHub-Event'];
 
-  // check that a GitHub webhook secret variable exists, if not, return an error
   if (typeof token !== 'string') {
     let errMsg = 'Must provide a \'GITHUB_WEBHOOK_SECRET\' env variable';
     return callback(null, {
@@ -18,14 +17,12 @@ exports.githubWebhookListener = (event, context, callback) => {
     });
   }
 
-  // print some messages to the CloudFormation console
   console.log('---------------------------------');
   console.log(`Github-Event: "${githubEvent}" on this repo: "${repo}" at the url: ${url}.\n ${username} is to blame if something was broken.`);
   console.log('---------------------------------');
   console.log(event.body);
   console.log('---------------------------------');
 
-  // return a 200 response if the GitHub tokens match
   const response = {
     statusCode: 200,
     body: JSON.stringify({
