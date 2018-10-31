068392c41f0fc5e6ccdb85acc7cd1166ed0126c0
Anna Spysz <anna.spysz@stackery.io>
2018-10-31 15:20:54 -0700

    stuff

--- src/triggerDeploy/index.js
+++ src/triggerDeploy/index.js
@@ -1,13 +1,28 @@
-module.exports.githubWebhookListener = (event, context, callback) => {
-  const token = process.env.GITHUB_WEBHOOK_SECRET;
-  const headers = event.headers;
-  const body = JSON.parse(event.body);
-  const { repository, sender } = body;
-  const repo = repository.name;
-  const username = sender.login;
-  const url = repository.url;
-  const githubEvent = headers['X-GitHub-Event'];
+const stackery = require('stackery')
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const CwLogs = require('aws-cwlogs');
 
+module.exports.githubWebhookListener = (event, context, callback) => {
+  // get environment variables
+  const stackeryKey = process.env.STACKERY_KEY;
+  const stackerySecret = process.env.STACKERY_SECRET;
+
+  // get the GitHub secret from the environment variables
+  const token = process.env.GITHUB_WEBHOOK_SECRET;
+  // get the remaining variables from the GitHub event
+  const headers = event.headers;
+  const githubEvent = headers['X-GitHub-Event'];
+  const body = JSON.parse(event.body);
+  // this prevents errors from the GitHub ping event
+  const username = body.pusher ? body.pusher.name : body.repository.owner.login;
+  const message = body.pusher ? `${username} pushed this awesomeness/atrocity through (delete as necessary)` : `Pinging ${username}'s repo.`
+  // get repo variables
+  const { repository } = body;
+  const repo = repository.full_name;
+  const url = repository.url;
+  
+  // check that a GitHub webhook secret variable exists, if not, return an error
   if (typeof token !== 'string') {
     let errMsg = 'Must provide a \'GITHUB_WEBHOOK_SECRET\' env variable';
     return callback(null, {
@@ -16,13 +31,30 @@ module.exports.githubWebhookListener = (event, context, callback) => {
       body: errMsg,
     });
   }
+  
+  // install stackery with configs
 
+  // make or overwrite .stackery.toml
+  // will look like:
+  // [stackery]
+  //   key = "1rUx**********************T7"
+  //   secret = "Kojh**********************Da0r3m9"
+
+  // deploy 
+
+
+
+
+
+
+  // We'll keep logging to CloudWatch in case we need to debug
   console.log('---------------------------------');
-  console.log(`Github-Event: "${githubEvent}" on this repo: "${repo}" at the url: ${url}.\n ${username} is to blame if something was broken.`);
+  console.log(`Github-Event: "${githubEvent}" on this repo: "${repo}" at the url: ${url}.\n ${message}`);
   console.log('---------------------------------');
   console.log(event.body);
   console.log('---------------------------------');
 
+  // return a 200 response if the GitHub tokens match
   const response = {
     statusCode: 200,
     body: JSON.stringify({
